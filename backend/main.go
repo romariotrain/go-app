dfd0d9f16e6147ead980c0d5d7c8b5452712d457
Your Name <you@example.com>
2025-12-09 11:53:04 +0000

    commit

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"sync"
 	"time"
+	"math/big"
 )
 
 // Post представляет структуру записи в блоге
@@ -51,8 +52,19 @@ func enableCORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
+func factorial(n int64) *big.Int {
+	result := big.NewInt(1)
+	for i := int64(2); i <= n; i++ {
+		result.Mul(result, big.NewInt(i))
+	}
+	return result
+}
+
 // Обработчик для получения всех постов
 func getPosts(w http.ResponseWriter, r *http.Request) {
+	factorial(2000)
+	w.Write([]byte("ok"))
+
 	if r.Method == "OPTIONS" {
 		enableCORS(w)
 		return
